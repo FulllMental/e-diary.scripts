27085ecf7f63b083cc028e5e0b8a5d65a800adf6
FulllMental <104234625+FulllMental@users.noreply.github.com>
2022-11-25 01:36:20 +0300

    Change queries

--- scripts.py
+++ scripts.py
@@ -1,14 +1,17 @@
-from datacenter import models
 import random
+
+from environs import Env
+
+from datacenter.models import Schoolkid, Mark, Chastisement, Lesson, Commendation
 
 
 def find_schoolkid(schoolkid):
     try:
-        kid = models.Schoolkid.objects.get(full_name__contains=schoolkid)
+        kid = Schoolkid.objects.get(full_name__contains=schoolkid)
         return kid
-    except models.Schoolkid.DoesNotExist:
+    except Schoolkid.DoesNotExist:
         print('Ученик с таким именем не найден...')
-    except models.Schoolkid.MultipleObjectsReturned:
+    except Schoolkid.MultipleObjectsReturned:
         print('Слишком много совпадений! Уточните ФИО ученика...')
 
 
@@ -17,7 +20,7 @@ def fix_marks(schoolkid):
     if not kid:
         print('Из-за ошибки поиска ученика дальше программа выполниться не может...')
         return
-    models.Mark.objects.filter(schoolkid=kid, points__lte=3).update(points=5)
+    Mark.objects.filter(schoolkid=kid, points__lte=3).update(points=5)
     print('Все плохие оценки исправлены...')
 
 
@@ -26,56 +29,29 @@ def remove_chastisements(schoolkid):
     if not kid:
         print('Из-за ошибки поиска ученика дальше программа выполниться не может...')
         return
-    schoolkid_chastisements = models.Chastisement.objects.filter(schoolkid=kid)
+    schoolkid_chastisements = Chastisement.objects.filter(schoolkid=kid)
     schoolkid_chastisements.delete()
     print('Все замечания ученика удалены...')
 
 
 def create_commendation(schoolkid, subject):
-    good_commendations = [
-        'Молодец!',
-        'Отлично!',
-        'Хорошо!',
-        'Гораздо лучше, чем я ожидал!',
-        'Ты меня приятно удивил!',
-        'Великолепно!',
-        'Прекрасно!',
-        'Ты меня очень обрадовал!',
-        'Именно этого я давно ждал от тебя!',
-        'Сказано здорово – просто и ясно!',
-        'Ты, как всегда, точен!',
-        'Очень хороший ответ!',
-        'Талантливо!',
-        'Ты сегодня прыгнул выше головы!',
-        'Я поражен!',
-        'Уже существенно лучше!',
-        'Потрясающе!',
-        'Замечательно!',
-        'Прекрасное начало!',
-        'Так держать!',
-        'Ты на верном пути!',
-        'Здорово!',
-        'Это как раз то, что нужно!',
-        'Я тобой горжусь!',
-        'С каждым разом у тебя получается всё лучше!',
-        'Мы с тобой не зря поработали!',
-        'Я вижу, как ты стараешься!',
-        'Ты растешь над собой!',
-        'Ты многое сделал, я это вижу!',
-        'Теперь у тебя точно все получится!'
-    ]
+    env = Env()
+    env.read_env()
+    good_commendations = env.list('COMMENDATIONS')
     commedation_text = random.choice(good_commendations)
 
     kid = find_schoolkid(schoolkid)
     if not kid:
         print('Из-за ошибки поиска ученика дальше программа выполниться не может...')
         return
-    lessons = models.Lesson.objects.filter(year_of_study=kid.year_of_study, group_letter=kid.group_letter)
+    lessons = Lesson.objects.filter(year_of_study=kid.year_of_study, group_letter=kid.group_letter)
     subject_lessons = lessons.filter(subject__title=subject)
+    if subject_lessons.count() == 0:
+        print('Либо нет такого предмета, либо предмет написан с ошибкой...')
+        return
     random_lesson = random.choice(subject_lessons)
     lesson_date = random_lesson.date
     lesson_subject = random_lesson.subject
     lesson_teacher = random_lesson.teacher
-    commendations = models.Commendation.objects.filter(teacher=lesson_teacher, subject=lesson_subject, schoolkid=kid)
-    commendations.create(text=commedation_text, created=lesson_date, schoolkid=kid, subject=lesson_subject, teacher=lesson_teacher)
-    print(f'Похвала ученику добавлена...')
+    Commendation.create(text=commedation_text, created=lesson_date, schoolkid=kid, subject=lesson_subject, teacher=lesson_teacher)
+    print('Похвала ученику добавлена...')
